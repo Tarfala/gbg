e750f8919a7ecaa0b36398a6766716fda6b8c09b
Oscar Olsson <happybits@gmail.com>
2018-12-05 13:03:57 +0100

    Methodsandlists

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/NumberListToNumberList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodsAndLists.Core
+{
+    public class NumberListToNumberList
+    {
+
+        public List<int> Add70ToEverySecondElement(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> Add50ToFirstThreeElements(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> NegateEachNumber(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> DivideEachNumberBy100(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetNumbersDividableByFive(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetNumbersHigherThan1000(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> Add100ToEachNumber(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
