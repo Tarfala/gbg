14ed6924662e293bab6ea51e9f9f6ca93b201ddc
Oscar Olsson <happybits@gmail.com>
2018-12-06 14:22:19 +0100

    no message

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/StringToObjectList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using MethodsAndLists.Core.Models;
+
+namespace MethodsAndLists.Core
+{
+    public class StringToObjectList
+    {
+        public List<City> ParseCities(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
