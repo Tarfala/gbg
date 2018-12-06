25eb7ea2b54329c9bd842c12ac7ddd649fabd650
Oscar Olsson <happybits@gmail.com>
2018-12-06 13:02:37 +0100

    no message

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/NumberToNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace MethodsAndLists.Core
+{
+    public class NumberToNumber
+    {
+        public int SumNumbersTo(int input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumNumbersTo_WithoutLinq(int input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumNumbers(int from, int to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumNumbersDividedByThreeOrFive(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
