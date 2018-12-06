b60a2958d44c1a45bc81bafcc6e5854ce83b3b70
Oscar Olsson <happybits@gmail.com>
2018-12-06 14:24:05 +0100

    no message

--- C#/MethodsAndLists/MethodsAndLists.Core/NumberToNumber.cs
+++ C#/MethodsAndLists/MethodsAndLists.Core/NumberToNumber.cs
@@ -7,22 +7,50 @@ namespace MethodsAndLists.Core
     {
         public int SumNumbersTo(int input)
         {
-            throw new NotImplementedException();
+            if (input <= 0)
+                throw new ArgumentException();
+
+            return Enumerable.Range(1, input).Sum();
+        }
+
+        public int SumNumbersTo_OneLineOfCode(int input)
+        {
+            return input <= 0 ? throw new ArgumentException() : Enumerable.Range(1, input).Sum();
         }
 
         public int SumNumbersTo_WithoutLinq(int input)
         {
-            throw new NotImplementedException();
+            if (input <= 0)
+                throw new ArgumentException();
+
+            int result = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                result += i;
+            }
+
+            return result;
         }
 
         public int SumNumbers(int from, int to)
         {
-            throw new NotImplementedException();
+            if (to < from)
+                throw new ArgumentException();
+
+            return Enumerable.Range(from, to - from + 1).Sum();
         }
 
         public int SumNumbersDividedByThreeOrFive(int input)
         {
-            throw new NotImplementedException();
+            if (input <= 0)
+                throw new ArgumentException();
+
+            return Enumerable.Range(1, input).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
         }
     }
 }
+// 90 / 3 = 30 med resten 0         90 % 3 = 0
+// 91 / 3 = 30 med resten 1         91 % 3 = 1
+// 92 / 3 = 30 med resten 2         92 % 3 = 2
+// 93 / 3 = 31 med reston 0         93 % 3 = 0
+
