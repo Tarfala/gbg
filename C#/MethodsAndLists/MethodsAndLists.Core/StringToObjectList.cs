6c160a1cfd6d4ee0ea2a8885185db5e56c66e38f
Oscar Olsson <happybits@gmail.com>
2018-12-06 15:44:03 +0100

    ParseCity

--- C#/MethodsAndLists/MethodsAndLists.Core/StringToObjectList.cs
+++ C#/MethodsAndLists/MethodsAndLists.Core/StringToObjectList.cs
@@ -8,7 +8,28 @@ namespace MethodsAndLists.Core
     {
         public List<City> ParseCities(string input)
         {
-            throw new NotImplementedException();
+            if (input == null)
+                throw new ArgumentException();
+
+            var result = new List<City>();
+
+            if (input.Trim() == "")
+                return result;
+
+            string[] cityStringList = input.Split(";");
+
+            foreach (string cityString in cityStringList)
+            {
+                string name = cityString.Split(',')[0];
+                int population = int.Parse(cityString.Split(',')[1]);
+                var city = new City
+                {
+                    Name = name,
+                    Population = population
+                };
+                result.Add(city);
+            }
+            return result;
         }
     }
 }
