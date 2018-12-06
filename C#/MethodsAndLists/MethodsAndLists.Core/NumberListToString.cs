7e07f609eafc96e340fae2935b24246c8237b595
Oscar Olsson <happybits@gmail.com>
2018-12-06 08:54:37 +0100

    no message

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/NumberListToString.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MethodsAndLists.Core
+{
+    public class NumberListToString
+    {
+        // Demo: returnera "fjärde talet är jättestort" om det är större än 10000
+
+        public string ReportNumberOfNegativeValues(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReportIfAllValuesAreHigherThan100(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReportFirstAndLastValue(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReportFirstAndLastValue_Linq(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- C#/MethodsAndLists/MethodsAndLists.Core/NumberListToStringList.cs
+++ C#/MethodsAndLists/MethodsAndLists.Core/NumberListToStringList.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System;
 
 namespace MethodsAndLists.Core
 {
@@ -8,22 +7,78 @@ namespace MethodsAndLists.Core
 
         public List<string> LongOrShort(List<int> heightList)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            foreach (var height in heightList)
+            {
+                string newString;
+
+                if (height < 0 || height > 220)
+                    continue;
+
+                if (height < 160)
+                    newString = height + "cm är kort";
+                else
+                    newString = height + "cm är långt";
+
+                result.Add(newString);
+            }
+
+            return result;
         }
 
         public List<string> NegativeNumbersIsZip_PositiveNumbersIsZap_ZeroIsBoing(List<int> numbers)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            foreach (var number in numbers)
+            {
+                string newString;
+
+                if (number == 0)
+                {
+                    newString = "BOING";
+                }
+                else if (number < 0)
+                {
+                    newString = "ZIP";
+                }
+                else
+                {
+                    newString = "ZAP";
+                }
+
+                result.Add(newString);
+            }
+
+            return result;
         }
 
         public List<string> AddStarsToNumbersHigherThan1000(List<int> numbers)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            foreach (var number in numbers)
+            {
+                string newString = number.ToString();
+                if (number > 1000)
+                {
+                    newString = "***" + number + "***";
+                }
+
+                result.Add(newString);
+            }
+
+            return result;
         }
 
         public List<string> AddStars(List<int> numbers)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            foreach (var number in numbers)
+            {
+                string newString = "***" + number + "***";
+                result.Add(newString);
+            }
+
+            return result;
         }
     }
 }
