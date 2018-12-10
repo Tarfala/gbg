8f7eda7ed3c13baa22d0dd48087ae98b3862903d
Oscar Olsson <happybits@gmail.com>
2018-12-10 11:12:13 +0100

    no message

--- C#/MethodsAndLists/MethodsAndLists.Core/StringListToStringList.cs
+++ C#/MethodsAndLists/MethodsAndLists.Core/StringListToStringList.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MethodsAndLists.Core
 {
@@ -10,7 +8,55 @@ namespace MethodsAndLists.Core
     {
         public IEnumerable<string> GetEverySecondElement(string[] strings)
         {
-            throw new NotImplementedException();
+            if (strings == null)
+                throw new ArgumentNullException();
+
+            var result = new List<string>();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i % 2 == 1)
+                    result.Add(strings[i]);
+            }
+            return result;
         }
+
+        public IEnumerable<string> GetEverySecondElement_WithYield(string[] strings)
+        {
+            if (strings == null)
+                throw new ArgumentNullException();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (i % 2 == 1)
+                    yield return strings[i];
+            }
+        }
+
+        public IEnumerable<string> GetEverySecondElement_OneLiner(string[] strings)
+        {
+            if (strings == null)
+                throw new ArgumentNullException();
+
+            return strings.Where((x, index) => index % 2 == 1);
+
+        }
+
+        // Lösning med hjälpmetod:
+
+        public IEnumerable<string> GetEverySecondElement_OneLiner_WithHelpMethod(string[] strings)
+        {
+            if (strings == null)
+                throw new ArgumentNullException();
+
+            return OddNumbersTo(strings.Length).Select(index => strings[index]);
+
+        }
+
+        public IEnumerable<int> OddNumbersTo(int lastvalue)
+        {
+            return Enumerable.Range(0, lastvalue).Where(index => index % 2 == 1);
+        }
+
     }
 }
