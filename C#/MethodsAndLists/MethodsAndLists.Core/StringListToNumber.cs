d32434ecb3d0c836aa7b2153a9aebf85a8def292
Oscar Olsson <happybits@gmail.com>
2018-12-10 08:57:04 +0100

    no message

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/StringListToNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndLists.Core
+{
+    public class StringListToNumber
+    {
+        public int ElevatorGoUpAndDown(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ElevatorGoUpAndDown_Linq(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
