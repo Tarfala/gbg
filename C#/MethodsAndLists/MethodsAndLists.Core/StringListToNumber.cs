5af02f1eeebd5451563b9c90bcfed48522302bbb
Oscar Olsson <happybits@gmail.com>
2018-12-10 09:59:44 +0100

    no message

--- C#/MethodsAndLists/MethodsAndLists.Core/StringListToNumber.cs
+++ C#/MethodsAndLists/MethodsAndLists.Core/StringListToNumber.cs
@@ -1,21 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MethodsAndLists.Core
 {
     public class StringListToNumber
     {
-        public int ElevatorGoUpAndDown(string[] input)
+        // Demo: räkna antalet tecken i alla strängar tillsammans
+
+        public int ElevatorGoUpAndDown(IEnumerable<string> list)
         {
-            throw new NotImplementedException();
+            if (list == null)
+                return 0;
+
+            int floorNumber = 0;
+            foreach (string command in list)
+            {
+                if (command == "UPP")
+                    floorNumber++;
+                if (command == "NER")
+                    floorNumber--;
+            }
+
+            return floorNumber;
         }
 
-        public int ElevatorGoUpAndDown_Linq(string[] input)
+        public int ElevatorGoUpAndDown_Linq(IEnumerable<string> list)
         {
-            throw new NotImplementedException();
+            if (list == null)
+                return 0;
+
+            return list.Count(x => x == "UPP") - list.Count(x => x == "NER");
+        }
+
+        public int ElevatorGoUpAndDown_Linq_OneLiner(IEnumerable<string> list)
+
+        {
+            if (list == null)
+                return 0;
+
+            return list.Sum(x => x == "UPP" ? 1 : x == "NER" ? -1 : 0);
         }
     }
 }
