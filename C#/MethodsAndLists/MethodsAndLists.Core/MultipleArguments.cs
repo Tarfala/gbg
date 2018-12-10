7a55e7d22284297bf0a47038e351c5bea344dccb
Oscar Olsson <happybits@gmail.com>
2018-12-10 11:22:00 +0100

    no message

---
+++ C#/MethodsAndLists/MethodsAndLists.Core/MultipleArguments.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MethodsAndLists.Core.Enums;
+
+namespace MethodsAndLists.Core
+{
+    public class MultipleArguments
+    {
+        public List<string> SomeToUpper(List<string> list, List<bool> toUpper)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<double> MultiplyAllBy(int factor, List<double> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<double> MultiplyAllBy_Linq(int factor, List<double> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> NearbyElements(int position, List<string> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<int>> MultiplicationTable(int rowMax, int colMax)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<int>> MultiplicationTable_Linq(int rowMax, int colMax)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ComputeSequenceSumOrProduct(int toNumber, bool sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ComputeSequence(int toNumber, ComputeMethod sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] CombineLists(int[] list1, int[] list2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] RotateList(int[] list, int rotation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
